24a9961c8c6aff5a1f339bfa1caa9b96589fb96c
Neel Rana <neelrana1418@gmail.com>
2026-08-25 00:50:43 +0530

    Save theme preference with local storage

--- script.js
+++ script.js
@@ -199,8 +199,20 @@ transactionList.addEventListener("click", function(event) {
 
 // DARK MODE
 
+// DARK MODE
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+}
+
 themeToggle.addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
 });
 
 
